2cdc60282457cf012ba64f1c9571081e381ee273
ivb98 <ivanvalette@hotmail.com>
2020-05-24 12:42:07 -0400

    Add optimization to matrix multiplication

--- lib/main.c
+++ lib/main.c
@@ -10,10 +10,6 @@ float EMSCRIPTEN_KEEPALIVE sum_up(float *vals, int size) {
   return res;
 }
 
-int EMSCRIPTEN_KEEPALIVE test(){
-  return 30;
-}
-
 void EMSCRIPTEN_KEEPALIVE printArr(int *m, int length){
   for(int i = 0; i < length; i++){
     m[i] = 30;
@@ -24,7 +20,7 @@ void EMSCRIPTEN_KEEPALIVE ijk(int* matrixA, int* matrixB, int* matrixC, int leng
   for(unsigned int i = 0; i < length; i++){
     for(unsigned int j = 0; j < length; j++) {
       for(unsigned int k = 0; k < length; k++) {
-        matrixC[i * length + j] = matrixA[i * length + j];
+        matrixC[i * length + j] += (matrixA[i * length + k] * matrixB[j * length + k]);
       }
     }
   }
